b70618f99a08a726b02757435cc8a43be028bdd3
Arnau Siches <arnau@ustwo.com>
2016-03-07 17:46:36 +0000

    Concat request results

--- src/cmd/fetch.rs
+++ src/cmd/fetch.rs
@@ -55,16 +55,20 @@ pub fn run(owner: &str,
     let page = 1;
     let url = format!("https://api.github.com/repos/{}/{}/issues?state=all&page={}",
                       owner, repo, page);
+    println!("Fetching {:?}", url);
     let res = get_page(url, &oauth_token);
     let body = match str::from_utf8(res.get_body()) {
         Ok(b) => b,
         Err(..) => "Unable to parse"
     };
+    let issues_result: Result<Vec<Issue>,_> = json::decode(body);
+    let mut issues = issues_result.unwrap();
+    // println!("{:?}", issues);
 
     match res.get_headers().get("link") {
         Some(links) => {
             let mut nurl = next_url(links[0].clone());
-            println!("{:?}", nurl);
+            println!("Fetching {:?}", nurl);
             while let Some(nu) = nurl {
                 println!("{:?}", nu);
                 let r = get_page(nu.to_string(), &oauth_token);
@@ -72,24 +76,14 @@ pub fn run(owner: &str,
                     Ok(b) => b,
                     Err(..) => "Unable to parse"
                 };
+                let iss_result: Result<Vec<Issue>,_> = json::decode(b);
+                issues.extend(iss_result.unwrap());
                 let link = r.get_headers().get("link").unwrap()[0].clone();
                 nurl = next_url(link);
             }
         }
         _ => {}
     }
-
-    // for (k, v) in res.get_headers() {
-    //     println!("{:?}: {:?}", k, v);
-    // }
-
-    // println!("code={}; headers={:?}",
-    //          res.get_code(),
-    //          res.get_headers());
-
-    let issues_result: Result<Vec<Issue>,_> = json::decode(body);
-    let issues = issues_result.unwrap();
-    // println!("{:?}", issues);
 
 
     // let mut wtr = csv::Writer::from_memory();
