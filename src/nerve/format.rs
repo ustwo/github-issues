9764885a0e78ccb10ece9a56a86f52e0617b9ded
Arnau Siches <arnau@ustwo.com>
2016-11-02 14:44:57 +0000

    test: Add basic tests for split()
    Signed-off-by: Arnau Siches <arnau@ustwo.com>

--- src/nerve/format.rs
+++ src/nerve/format.rs
@@ -16,3 +16,47 @@ impl FromStr for OutputFormat {
         }
     }
 }
+
+
+// CSV lib does not cast Vec<String> so this is a workaround.
+pub fn split(s: String) -> Vec<String> {
+    let s = s.trim();
+
+    if s.is_empty() {
+        vec![]
+    } else {
+        s.split(",").map(|s| From::from(s.trim())).collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_string() {
+        let expected: Vec<String> = vec![];
+        assert_eq!(split("".to_string()), expected);
+    }
+
+    #[test]
+    fn string_with_spaces() {
+        let expected: Vec<String> = vec![];
+        assert_eq!(split("  ".to_string()), expected);
+    }
+
+    #[test]
+    fn string_with_one_item() {
+        let expected: Vec<String> = vec![String::from("one")];
+        assert_eq!(split("one".to_string()), expected);
+    }
+
+    #[test]
+    fn string_with_multiple_item() {
+        let expected: Vec<String> = vec![ String::from("one")
+                                        , String::from("two")
+                                        , String::from("three") ];
+        assert_eq!(split("one,two, three".to_string()), expected);
+    }
+
+}
